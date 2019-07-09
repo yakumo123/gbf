dbf9f087d1b6b24beae35059c8d0be7415922f03
yakumo123 <52350839+yakumo123@users.noreply.github.com>
2019-07-09 15:34:17 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -7,8 +7,6 @@ require('fs').readdirSync('./roll/').forEach(function(file) {
 	}
   });
 
-//用來呼叫骰組,新增骰組的話,要寫條件式到下面呼叫 
-//格式是 exports.骰組檔案名字.function名
 function parseInput(rplyToken, inputStr) {
 	//console.log('InputStr: ' + inputStr);
 	_isNaN = function(obj) 	{
@@ -19,22 +17,16 @@ function parseInput(rplyToken, inputStr) {
 	let mainMsg = inputStr.match(msgSplitor); //定義輸入字串
 	let trigger = mainMsg[0].toString().toLowerCase(); //指定啟動詞在第一個詞&把大階強制轉成細階
 
-	//普通ROLL擲骰判定在此	
 	if (inputStr.match(/\w/)!=null && inputStr.toLowerCase().match(/\d+d+\d/)!=null) return exports.rollbase.nomalDiceRoller(inputStr,mainMsg[0],mainMsg[1],mainMsg[2]);
 
-	//xBy>A 指令開始於此
 	if (trigger.match(/^(\d+)(b)(\d+)$/i)!= null) return exports.advroll.xBy(trigger,mainMsg[1],mainMsg[2]);
-	//xUy 指令開始於此	
 	if (trigger.match(/^(\d+)(u)(\d+)$/i)!= null && isNaN(mainMsg[1])== false) return exports.advroll.xUy(trigger,mainMsg[1],mainMsg[2],mainMsg[3]);
 	
 	if (trigger.match(/^ccb$|^cc$|^ccn[1-2]$|^cc[1-2]$/)!= null && mainMsg[1]<=1000 )
 	{		
-	//ccb指令開始於此
 	if (trigger == 'ccb'&& mainMsg[1]<=99) return exports.coc.coc6(mainMsg[1],mainMsg[2]);
 	
-	//cc指令開始於此
 	if (trigger == 'cc'&& mainMsg[1]<=1000) return exports.coc.coc7(mainMsg[1],mainMsg[2]);
-	//獎懲骰設定於此	
 	if (trigger == 'cc1'&& mainMsg[1]<=1000) return exports.coc.coc7bp(mainMsg[1],'1',mainMsg[2]); 
 	if (trigger == 'cc2'&& mainMsg[1]<=1000) return exports.coc.coc7bp(mainMsg[1],'2',mainMsg[2]);	
 	if (trigger == 'ccn1'&& mainMsg[1]<=1000) return exports.coc.coc7bp(mainMsg[1],'-1',mainMsg[2]);	
@@ -55,18 +47,14 @@ function parseInput(rplyToken, inputStr) {
 	//nc指令開始於此 來自Rainsting/TarotLineBot 
 	if (trigger.match(/^[1-4]n[c|a][+|-][1-99]$|^[1-4]n[c|a]$/)!= null ) return exports.nc.nechronica(trigger,mainMsg[1]);
 
-	//依戀
 	if (trigger.match(/(^nm$)/) != null)	return exports.nc.nechronica_mirenn(mainMsg[1]);
 
 	
-	//wod 指令開始於此
 	if (trigger.match(/^(\d+)(wd|wod)(\d|)((\+|-)(\d+)|)$/i)!= null)return exports.wod.wod(trigger,mainMsg[1]);
 
-	//Dx3 指令開始於此
 	if (trigger.match(/^(\d+)(dx)(\d|)(((\+|-)(\d+)|)((\+|-)(\d+)|))$/i)!= null)return exports.dx3.dx(trigger);
 
-	//Fisher–Yates shuffle
- 	//SortIt 指令開始於此
+
  	if (trigger.match(/排序|次序/)!= null && mainMsg.length >= 3) return exports.funny.SortIt(inputStr,mainMsg);
  	if (trigger.match(/^d66$/)!= null ) return exports.advroll.d66(mainMsg[1]);
 	if (trigger.match(/^d66s$/)!= null ) return exports.advroll.d66s(mainMsg[1]);
@@ -83,7 +71,6 @@ function parseInput(rplyToken, inputStr) {
 	if (trigger.match(/選8/)!= null && mainMsg.length >= 3) return exports.funny.eightchoice(inputStr,mainMsg);
 	if (trigger.match(/選9/)!= null && mainMsg.length >= 3) return exports.funny.ninechoice(inputStr,mainMsg);
 
-	//tarot 指令
 	if (trigger.match(/tarot|塔羅牌|塔羅/) != null) {
 		if (trigger.match(/^單張|^每日|^daily/)!= null) return exports.funny.NomalDrawTarot(mainMsg[1], mainMsg[2]);//預設抽 79 張
 		if (trigger.match(/^時間|^time/)!= null) 	return exports.funny.MultiDrawTarot(mainMsg[1], mainMsg[2], 1);
