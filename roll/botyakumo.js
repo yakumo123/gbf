b997c87eb7dcdd8dc0930a44ed63ca623936e2e9
yakumo123 <52350839+yakumo123@users.noreply.github.com>
2019-07-09 14:56:48 +0800

    Add files via upload

--- roll/botyakumo.js
+++ roll/botyakumo.js
@@ -5,7 +5,7 @@ var rply = { type: 'text' }; //type是必需的,但可以更改
 function BotHelpYy() {
 	rply = {
 		"type": "template",
-		"altText": "【教學】",
+		"altText": "【式神的教學】",
 		"template": {
 			"type": "carousel",
 			"columns": [
@@ -15,24 +15,25 @@ function BotHelpYy() {
 					"actions": [
 						{
 							"type": "message",
-							"label": "GBF情報",
-							"text": "gbf情報"
+							"label": "sv情報",
+							"text": "sv情報"
 						},
 						{
 							"type": "message",
-							"label": "NA",
-							"text": "NA"
+							"label": "公主連結情報",
+							"text": "公主情報"
 						},
 						{
 							"type": "message",
-							"label": "NA",
-							"text": "NA"
+							"label": "～式神幫助2～",
+							"text": "式神幫助2"
 						}
+
 					]
 				},
 				{
-					"title": "《其他功能》",
-					"text": "《其他功能》",
+					"title": "《隨機選擇/生產亂數及排序系統》",
+					"text": "隨機選擇功能",
 					"actions": [
 						{
 							"type": "message",
@@ -53,13 +54,35 @@ function BotHelpYy() {
 					]
 				},
 				{
-					"title": "《其他功能》",
-					"text": "《其他功能》",
+					"title": "《自創人物角色系統/TRPG功能》",
+					"text": "角色背景/TRPG功能",
 					"actions": [
 						{
 							"type": "message",
 							"label": "角色背景示範",
 							"text": "八雲的角色背景 \n使用方法為 XXX的角色背景"
+						},
+						{
+							"type": "message",
+							"label": "不要按我",
+							"text": "我想被轟成智障呢~~"
+						},
+						{
+							"type": "message",
+							"label": "TRPG功能",
+							"text": "trpghelp"
+						}
+
+					]
+				},
+				{
+					"title": "《運勢及塔羅占卜功能》",
+					"text": "占卜功能",
+					"actions": [
+						{
+							"type": "message",
+							"label": "運勢占卜範例",
+							"text": "求八雲式神運勢 \n使用方法為 XXX運勢"
 						},
 						{
 							"type": "message",
@@ -75,14 +98,9 @@ function BotHelpYy() {
 					]
 				},
 				{
-					"title": "《其他功能》",
-					"text": "《其他功能》",
+					"title": "《隨機生成》",
+					"text": "隨機生成",
 					"actions": [
-						{
-							"type": "message",
-							"label": "運勢占卜範例",
-							"text": "求八雲式神運勢 \n使用方法為 XXX運勢"
-						},
 						{
 							"type": "message",
 							"label": "能力值生成(可不加歲數)",
@@ -91,13 +109,18 @@ function BotHelpYy() {
 						{
 							"type": "message",
 							"label": "綜合分數生成",
-							"text": "單張塔羅/大十字塔羅/每日塔羅牌/時間tarot 單張,大十字,每日及時間必須放頭"
+							"text": "式神的綜合分數 \n使用方法為 XXX的綜合分數"
+						},
+						{
+							"type": "message",
+							"label": "黑歷史隨機生產",
+							"text": "萌式神的黑歷史 \n使用方法為 XXX的黑歷史"
 						}
 
 					]
 				},
 				{
-					"title": "《隨機生成》",
+					"title": "《隨機建議生成》",
 					"text": "隨機生成",
 					"actions": [
 						{
@@ -112,8 +135,8 @@ function BotHelpYy() {
 						},
 						{
 							"type": "message",
-							"label": "黑歷史隨機生產",
-							"text": "萌式神的黑歷史 \n使用方法為 XXX的黑歷史"
+							"label": "生成卡片",
+							"text": "生成卡片 八雲 \n教學：生成卡片 名字"
 						}
 
 					]
@@ -129,6 +152,27 @@ function BotHelpYy() {
 						},
 						{
 							"type": "message",
+							"label": "撲克",
+							"text": "撲克1-14/撲克大老二\n\n用法自行發掘，最高抽撲克功能只支援到14張。\n\n【例子1】撲克12\n\n【例子2】撲克大老二"
+						},
+						{
+							"type": "message",
+							"label": "偽式神幫助【毀其他人形象用】",
+							"text": "式神幫忙"
+						}
+					]
+				},
+				{
+					"title": "其他功能",
+					"text": "其他",
+					"actions": [
+						{
+							"type": "postback",
+							"label": "暫無",
+							"data": "暫無"
+						},
+						{
+							"type": "message",
 							"label": "機率提供",
 							"text": "八雲運氣變好的機率 ■XXX+的機率"
 						},
@@ -137,6 +181,28 @@ function BotHelpYy() {
 							"label": "【新】立Flag功能",
 							"text": "flag \n使用方法為XXXflag"
 						}				
+					]
+				},
+				{
+					"title": "《其他功能》",
+					"text": "其他功能",
+					"actions": [
+						{
+							"type": "postback",
+							"label": "暫無",
+							"data": "暫無"
+						},
+						{
+							"type": "postback",
+							"label": "暫無",
+							"data": "暫無"
+						},
+						{
+							"type": "postback",
+							"label": "暫無",
+							"data": "暫無"
+						}
+
 					]
 				}
 			]
