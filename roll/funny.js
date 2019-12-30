4f1997aeb63b5b537ad13be1dd72b63368795a3c
yakumo123 <52350839+yakumo123@users.noreply.github.com>
2019-12-30 12:29:49 +0800

    Update funny.js

--- roll/funny.js
+++ roll/funny.js
@@ -1,29 +1,6 @@
 var rollbase = require('./rollbase.js');
 var rply ={type : 'text'}; //type是必需的,但可以更改
 
-function RockPaperScissors(chack,text) {
-	let returnStr = '';
-	let handArr = [1,2,3]；
-	let hand = handArr[Math.floor((Math.random() * (handArr.length)) + 0)];
-	
-	if (TEXT[1] == 1 && hand == 1) returnStr += '我出石頭！\n哼哼你輸惹';
-	if (TEXT[1] == 2 && hand == 1) returnStr += '我出石頭！\n看來我們不相上下阿';
-	if (TEXT[1] == 3 && hand == 1) returnStr += '我出石頭！\n你好像有點強！';
-	if (TEXT[1] == 1 && hand == 2) returnStr += '我出布！\n讓你一次而已啦！';
-	if (TEXT[1] == 3 && hand == 2) returnStr += '我出布！\n原來平手...沒什麼嘛！';
-	if (TEXT[1] == 2 && hand == 2) returnStr += '我出布！\n哈哈你看看你！';
-	if (TEXT[1] == 1 && hand == 3) returnStr += '我出剪刀！\n平手 (  艸)';
-	if (TEXT[1] == 3 && hand == 3) returnStr += '我出剪刀！\n贏了 (｀・ω・´)b';
-	if (TEXT[1] == 2 && hand == 3) returnStr += '我出剪刀！\n輸惹 ゜。。゜(ノД‵)ノ・゜';
-	if (hand == 3) returnStr += '我出剪刀！\n亂出打你喔 (｀・ω・´)凸';
-	if (hand == 1) returnStr += '我出石頭！\n欸不對喔你亂出！';
-	if (hand == 2) returnStr += '我出布！\n別亂出阿會壞掉的';
-
-	}
-
-	rply.text = returnStr;
-	return rply;
-}
 
 function meat(TEXT) {
 let FirArr = [1,2,3,4,5,6,7,8,9]
@@ -525,7 +502,6 @@ function tenchoice(input,mainMsg) {
  }
 
 module.exports = {
-	RockPaperScissors,
 	meat,
 	randomColour,
 	randomDrink,
