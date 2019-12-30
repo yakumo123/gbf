345ddb9a12e6bab63df4e59e7473fa778bcf7e15
yakumo123 <52350839+yakumo123@users.noreply.github.com>
2019-12-30 12:20:51 +0800

    Update funny.js

--- roll/funny.js
+++ roll/funny.js
@@ -3,41 +3,36 @@ var rply ={type : 'text'}; //type是必需的,但可以更改
 
 function RockPaperScissors(chack,text) {
 	let returnStr = '';
-	var hand = rollbase.FunnyDice(3); // 0:石頭 1:布 2:剪刀
+	let handArr = [1,2,3]；
+	let hand = handArr[Math.floor((Math.random() * (handArr.length)) + 0)];
+	
 
-	switch (hand) {
-	case 0: 
-		returnStr = '我出石頭！\n';
+		if (TEXT[1] == 1 && hand == 1) returnStr += '我出石頭！\n哼哼你輸惹';
+		else
+			if (TEXT[1] == 2 && hand == 1) returnStr += '我出石頭！\n看來我們不相上下阿';
+		else
+			if (TEXT[1] == 3 && hand == 1) returnStr += '我出石頭！\n你好像有點強！';
+		else
+			if (TEXT[1] == 1 && hand == 2) returnStr += '我出布！\n讓你一次而已啦！';
+		else
+			if (TEXT[1] == 3 && hand == 2) returnStr += '我出布！\n原來平手...沒什麼嘛！';
+		else
+			if (TEXT[1] == 2 && hand == 2) returnStr += '我出布！\n哈哈你看看你！';
+		else
+			if (TEXT[1] == 1 && hand == 3) returnStr += '我出剪刀！\n平手 (  艸)';
+		else 
+			if (TEXT[1] == 3 && hand == 3) returnStr += '我出剪刀！\n贏了 (｀・ω・´)b';
+		else 
+			if (TEXT[1] == 2 && hand == 3) returnStr += '我出剪刀！\n輸惹 ゜。。゜(ノД‵)ノ・゜';
+		else 
+			if (TEXT[1] == null) returnStr = '我出的是...欸不對你沒出喔！\n';
+		else
+			if (hand == 3) eturnStr += '我出剪刀！\n亂出打你喔 (｀・ω・´)凸';
+		else
+			if (hand == 1) returnStr += '我出石頭！\n欸不對喔你亂出！';
+		else
+			if (hand == 2) returnStr += '我出布！\n別亂出阿會壞掉的';
 
-		if (TEXT[1] == 1) returnStr += '哼哼你輸惹';
-		else if (TEXT[1] == 2) returnStr += '看來我們不相上下阿';
-		else if (TEXT[1] == 3) returnStr += '你好像有點強！';
-		else returnStr += '欸不對喔你亂出！';
-
-		break;
-
-	case 1: 
-		returnStr = '我出布！\n';
-
-		if (TEXT[1] == 1) returnStr += '讓你一次而已啦！';
-		else if (TEXT[1] == 3) returnStr += '原來平手...沒什麼嘛！';
-		else if (TEXT[1] == 2) returnStr += '哈哈你看看你！';
-		else returnStr += '別亂出阿會壞掉的';
-
-		break;
-
-	case 2: 
-		returnStr = '我出剪刀！\n'; 
-		if (TEXT[1] == 1) returnStr += '平手 (  艸)';
-		else if (TEXT[1] == 3) returnStr += '贏了 (｀・ω・´)b';
-		else if (TEXT[1] == 2) returnStr += '輸惹 ゜。。゜(ノД‵)ノ・゜';
-		else returnStr += '亂出打你喔 (｀・ω・´)凸';
-
-		break;
-
-	default:
-		returnStr = '我出的是...欸不對你沒出喔！\n';
-		break;
 	}
 
 	rply.text = returnStr;
